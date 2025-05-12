47c43da80742c79286c2761b01b982ea2545abfb
lassandra85 <lassandra85@gmail.com>
2025-05-12 21:46:17 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+
+const widget = document.body;
+const colorValueSpan = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+function handleChangecolor() {
+    const newColor = getRandomHexColor();
+    widget.style.backgroundColor = newColor;
+    colorValueSpan.textContent = newColor;
+}
+
+changeColorBtn.addEventListener('click', handleChangecolor);
