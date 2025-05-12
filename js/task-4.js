ca35a149ec17a8bda8f99014ad4ada76657952b4
lassandra85 <lassandra85@gmail.com>
2025-05-12 21:28:30 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const emailValue = form.elements.email.value.trim();
+    const passwordValue = form.elements.password.value.trim();
+
+    if (!emailValue || !passwordValue) {
+        return alert('All form fields must be filled in');
+    }
+
+    const formData = {
+        email: emailValue,
+        password: passwordValue,
+    };
+
+    console.log(formData);
+
+    form.reset();
+}
